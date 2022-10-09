ae8fe40e26a7aaf41ceb0f8563d25095cef1b546
Simone Bottoni <39499778+SimoneBottoni@users.noreply.github.com>
2022-10-09 16:33:08 +0200

    feat: add AboutUs

--- src/components/about_us/AboutUs.js
+++ src/components/about_us/AboutUs.js
@@ -2,10 +2,7 @@ import about from '../../about-us.png';
 
 const AboutUs = () => {
     return(
-
-                <img src={about} alt="about-us" style={{ width: '100%', marginTop: '-4rem' }} />
-
-
+        <img src={about} alt="about-us" style={{ width: '100%', marginTop: '-4rem' }} />
     );
 }
 
